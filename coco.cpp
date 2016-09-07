2b6fa09fc481e552e7919045f8e3fe6906f618f0
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-09-08 04:53:05 +0900

    encapsulate all functions into CocoClient

--- coco.cpp
+++ coco.cpp
@@ -7,7 +7,7 @@
 #include <vector>
 #include <regex>
 
-struct Env {
+class CocoClient {
   std::vector<std::string> inputs;
   std::vector<std::string> render_items;
   std::vector<std::string> filtered;
@@ -16,170 +16,186 @@ struct Env {
   long cursor;
   size_t offset;
   size_t query_offset;
-};
 
-static Env env;
+  bool flag_selected = false;
 
-std::vector<std::string> filter()
-{
-  std::regex re(env.query);
+public:
+  CocoClient(std::vector<std::string> inputs) : inputs{std::move(inputs)}
+  {
+    tb_clear();
 
-  if (env.query.empty()) {
-    return env.inputs;
-  }
-  else {
-    std::vector<std::string> ret;
-    for (auto&& elem : env.inputs) {
-      if (std::regex_match(elem, re)) {
-        ret.push_back(elem);
-      }
-    }
-    return ret;
-  }
-}
-
-void update_query(bool init = false)
-{
-  std::string const query_header = "QUERY> ";
-
-  std::string print_query = query_header + env.query;
-
-  int const width = tb_width();
-
-  for (int x = 0; x < width; ++x) {
-    auto const c = static_cast<uint32_t>(x < print_query.length() ? print_query[x] : ' ');
-
-    if (x == print_query.length()) {
-      tb_change_cell(x, 0, c, TB_WHITE, TB_WHITE);
-    }
-    else {
-      tb_change_cell(x, 0, c, TB_WHITE, TB_BLACK);
-    }
+    update_query();
+    init();
   }
 
-  if (init) {
-    env.filtered = filter();
-    env.render_items = env.filtered;
-    env.selected = 0;
-    env.cursor = 0;
-    env.offset = 0;
-  }
-}
-
-void print(unsigned x, unsigned y, std::string line, bool selected)
-{
-  auto const c = static_cast<uint32_t>(x < line.length() ? line[x] : ' ');
-  constexpr unsigned y_offset = 1;
-
-  if (selected) {
-    tb_change_cell(x, y + y_offset, c, TB_RED, TB_WHITE);
-  }
-  else {
-    tb_change_cell(x, y + y_offset, c, TB_WHITE, TB_BLACK);
-  }
-}
-
-void update_items()
-{
-  tb_clear();
-  update_query();
-
-  for (int y = 0; y < (int)env.render_items.size(); ++y) {
-    for (int x = 0; x < tb_width(); ++x) {
-      print(x, y, env.render_items[y], y == env.selected);
-    }
-  }
-
-  tb_present();
-}
-
-int main()
-{
-  for (std::string line; std::getline(std::cin, line);) {
-    env.inputs.push_back(line);
-  }
-
-  if (int error = tb_init()) {
-    std::cerr << "tb_init() failed with error code " << error << std::endl;
-    return error;
-  }
-  tb_clear();
-
-  update_query(true);
-
-  bool selected = false;
-  for (bool quit = false; !quit;) {
-
+  bool poll_event()
+  {
     update_items();
 
     tb_event ev;
     tb_poll_event(&ev);
 
     if (ev.key == TB_KEY_ENTER) {
-      selected = true;
-      quit = true;
+      flag_selected = true;
+      return true;
     }
     else if (ev.key == TB_KEY_ESC) {
-      quit = true;
+      return true;
     }
     else if (ev.key == TB_KEY_BACKSPACE) {
-      if (!env.query.empty()) {
-        env.query.pop_back();
-        update_query(true);
+      if (!query.empty()) {
+        query.pop_back();
+        update_query();
+        init();
       }
     }
     else if (ev.key == TB_KEY_ARROW_UP) {
-      if (env.selected > -1) {
-        env.selected -= 1;
+      if (selected > -1) {
+        selected -= 1;
       }
-      if (env.cursor > 0) {
-        env.cursor -= 1;
+      if (cursor > 0) {
+        cursor -= 1;
       }
 
-      if (env.selected == -1) {
-        env.selected += 1;
-        if (env.offset > 0) {
-          env.offset -= 1;
+      if (selected == -1) {
+        selected += 1;
+        if (offset > 0) {
+          offset -= 1;
 
-          env.render_items.resize(env.filtered.size() - env.offset);
-          std::copy(env.filtered.begin() + env.offset, env.filtered.end(), env.render_items.begin());
+          render_items.resize(filtered.size() - offset);
+          std::copy(filtered.begin() + offset, filtered.end(), render_items.begin());
         }
       }
     }
     else if (ev.key == TB_KEY_ARROW_DOWN) {
-      if (env.cursor < env.render_items.size() - 1) {
-        env.cursor += 1;
+      if (cursor < render_items.size() - 1) {
+        cursor += 1;
       }
-      if ((env.render_items.size() < tb_height() - 1) && (env.selected < env.render_items.size() - 1)) {
-        env.selected += 1;
+      if ((render_items.size() < tb_height() - 1) && (selected < render_items.size() - 1)) {
+        selected += 1;
       }
-      else if ((env.render_items.size() > tb_height() - 1) && (env.selected < tb_height() - 1)) {
-        env.selected += 1;
+      else if ((render_items.size() > tb_height() - 1) && (selected < tb_height() - 1)) {
+        selected += 1;
       }
 
-      if (env.selected == tb_height() - 1) {
-        env.selected -= 1;
-        if (env.offset < env.filtered.size() - 1) {
-          env.offset += 1;
+      if (selected == tb_height() - 1) {
+        selected -= 1;
+        if (offset < filtered.size() - 1) {
+          offset += 1;
 
-          env.render_items.resize(env.filtered.size() - env.offset);
-          std::copy(env.filtered.begin() + env.offset, env.filtered.end(), env.render_items.begin());
+          render_items.resize(filtered.size() - offset);
+          std::copy(filtered.begin() + offset, filtered.end(), render_items.begin());
         }
       }
     }
     else {
       if ((ev.ch != 0) || (ev.key == 32 && ev.ch == 0)) {
-        env.query.push_back(ev.ch);
-        update_query(true);
+        query.push_back(ev.ch);
+        update_query();
+        init();
+      }
+    }
+    return false;
+  }
+
+  void describe() const
+  {
+    if (flag_selected) {
+      std::cout << render_items[offset + selected] << std::endl;
+    }
+  }
+
+private:
+  std::vector<std::string> filter()
+  {
+    std::regex re(query);
+
+    if (query.empty()) {
+      return inputs;
+    }
+    else {
+      std::vector<std::string> ret;
+      for (auto&& elem : inputs) {
+        if (std::regex_match(elem, re)) {
+          ret.push_back(elem);
+        }
+      }
+      return ret;
+    }
+  }
+
+  void update_query()
+  {
+    std::string const query_header = "QUERY> ";
+    std::string print_query = query_header + query;
+
+    for (int x = 0; x < tb_width(); ++x) {
+      auto const c = static_cast<uint32_t>(x < print_query.length() ? print_query[x] : ' ');
+      if (x == print_query.length()) {
+        tb_change_cell(x, 0, c, TB_WHITE, TB_WHITE);
+      }
+      else {
+        tb_change_cell(x, 0, c, TB_WHITE, TB_BLACK);
       }
     }
   }
 
-  tb_shutdown();
-
-  if (selected) {
-    std::cout << env.render_items[env.offset + env.selected] << std::endl;
+  void init()
+  {
+    filtered = filter();
+    render_items = filtered;
+    selected = 0;
+    cursor = 0;
+    offset = 0;
   }
 
+  void print(unsigned x, unsigned y, std::string line, bool selected)
+  {
+    auto const c = static_cast<uint32_t>(x < line.length() ? line[x] : ' ');
+    constexpr unsigned y_offset = 1;
+
+    if (selected) {
+      tb_change_cell(x, y + y_offset, c, TB_RED, TB_WHITE);
+    }
+    else {
+      tb_change_cell(x, y + y_offset, c, TB_WHITE, TB_BLACK);
+    }
+  }
+
+  void update_items()
+  {
+    tb_clear();
+    update_query();
+
+    for (int y = 0; y < (int)render_items.size(); ++y) {
+      for (int x = 0; x < tb_width(); ++x) {
+        print(x, y, render_items[y], y == selected);
+      }
+    }
+
+    tb_present();
+  }
+};
+
+int main()
+{
+  std::vector<std::string> inputs;
+  for (std::string line; std::getline(std::cin, line);) {
+    inputs.push_back(line);
+  }
+
+  if (int error = tb_init()) {
+    std::cerr << "tb_init() failed with error code " << error << std::endl;
+    return error;
+  }
+
+  auto cli = CocoClient(inputs);
+
+  while (!cli.poll_event()) {
+  }
+
+  tb_shutdown();
+
+  cli.describe();
   return 0;
 }

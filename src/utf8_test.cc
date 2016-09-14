a477def3e91a829dcefb6899a7b64a68e3cf8834
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-09-15 03:23:09 +0900

    fix test

--- src/utf8_test.cc
+++ src/utf8_test.cc
@@ -2,12 +2,47 @@
 
 #include "utf8.hh"
 
-TEST(utf8_test, is_utf8_first) {
+TEST(utf8_test, is_utf8_first)
+{
   EXPECT_TRUE(is_utf8_first('a'));
   EXPECT_TRUE(is_utf8_first('0'));
 
-  char hoge[] = "ほげ";
+  std::string hoge = u8"ほ";
   EXPECT_TRUE(is_utf8_first(hoge[0]));
 
   EXPECT_FALSE(is_utf8_first(0xA2));
+}
+
+TEST(utf8_test, is_utf8_cont)
+{
+  std::string hoge = u8"ほ";
+  EXPECT_FALSE(is_utf8_cont(hoge[0]));
+  EXPECT_TRUE(is_utf8_cont(hoge[1]));
+  EXPECT_TRUE(is_utf8_cont(hoge[2]));
+
+  EXPECT_FALSE(is_utf8_cont('a'));
+}
+
+TEST(utf8_test, get_utf8_char_length)
+{
+  std::string hoge = u8"ほ";
+  EXPECT_EQ(3, get_utf8_char_length(hoge[0]));
+
+  hoge = u8"🍣";
+  EXPECT_EQ(4, get_utf8_char_length(hoge[0]));
+}
+
+TEST(utf8_test, pop_back_utf8)
+{
+  std::string hoge = u8"ほげほaげ";
+
+  pop_back_utf8(hoge);
+  EXPECT_EQ(hoge, u8"ほげほa");
+
+  pop_back_utf8(hoge);
+  EXPECT_EQ(hoge, u8"ほげほ");
+
+  hoge += u8"🍣";
+  pop_back_utf8(hoge);
+  EXPECT_EQ(hoge, u8"ほげほ");
 }
